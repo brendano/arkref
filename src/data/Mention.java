58ae84ef862fc965f44126159ef5de07369250f8
mheilman <mheilman@08ee364a-b842-4d5f-b137-05461c37d77d>
2009-10-20 14:44:41 +0000

    use head to find entity type rather than right most token
    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@18 08ee364a-b842-4d5f-b137-05461c37d77d

--- src/data/Mention.java
+++ src/data/Mention.java
@@ -2,6 +2,8 @@ package data;
 
 import java.util.List;
 
+import parsestuff.AnalysisUtilities;
+
 import edu.stanford.nlp.trees.Tree;
 
 public class Mention {
@@ -15,10 +17,11 @@ public class Mention {
 	public Mention(int id, Sentence sentence, Tree node) { this.id=id; this.sentence=sentence; this.node=node; }
 	public String neType() {
 		// using head word strongly outperforms using right-most
-		List<Tree> leaves = node.getLeaves();
-		Tree rightmost = leaves.get(leaves.size()-1);
-//		System.out.println(rightmost);
-		return sentence.neType(rightmost);
+		//List<Tree> leaves = node.getLeaves();
+		//Tree rightmost = leaves.get(leaves.size()-1);
+		//return sentence.neType(rightmost);
+		Tree head = node.headTerminal(AnalysisUtilities.getInstance().getHeadFinder()); 
+		return sentence.neType(head);
 	}
 	public String toString() { 
 		return String.format("M%-3d : S%-2d : %-12s : %s", id, sentence.getID(), neType(), node); 
