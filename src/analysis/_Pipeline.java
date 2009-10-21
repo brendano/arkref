41bd1096813df614ddaf5301cb68fb2642b9a39c
brendano <brendano@08ee364a-b842-4d5f-b137-05461c37d77d>
2009-10-21 18:52:56 +0000

    ./summary.sh data/*.sent for a good time
    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@41 08ee364a-b842-4d5f-b137-05461c37d77d

--- src/analysis/_Pipeline.java
+++ src/analysis/_Pipeline.java
@@ -8,25 +8,18 @@ import data.Document;
 
 public class _Pipeline {
 	public static void main(String[] args) throws IOException {
-		String path;
-		if (args.length >= 1)  path = args[0];
-		else path = "/d/arkref/data/test1";
-	
-		if(!Preprocess.alreadyPreprocessed(path)){
-			Preprocess.go(path);
-		}		
-
-		Document d = Document.loadFiles(path);
-		
-		_Pipeline.go(d);
+		for (String path : args) {
+			if(!Preprocess.alreadyPreprocessed(path)){
+				Preprocess.go(path);
+			}
+			Document d = Document.loadFiles(path);
+			_Pipeline.go(d);
+		}	
 	}
 	
 	public static void go(Document d) throws IOException{
-
-	
 		FindMentions.go(d);
 		Resolve.go(d);
 		RefsToEntities.go(d);
-		
 	}
 }
