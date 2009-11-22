f89f8129396d3fd8537381b60f749067fc22b794
brendano <brendano@08ee364a-b842-4d5f-b137-05461c37d77d>
2009-11-22 20:46:11 +0000

    fix
    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@68 08ee364a-b842-4d5f-b137-05461c37d77d

--- servers.sh
+++ servers.sh
@@ -1,5 +1,5 @@
 set -x
 java -server -mx1g -cp bin:stanford-parser-2008-10-26.jar parsestuff.StanfordParserServer lib/englishPCFG.ser.gz &
-java -server -mx400m -cp stanford-ner-2008-05-07.jar edu.stanford.nlp.ie.NERServer -loadClassifier lib/ner-eng-ie.crf-muc7.ser.gz  1234 &
+java -server -mx400m -cp stanford-ner-2008-05-07.jar edu.stanford.nlp.ie.NERServer -loadClassifier lib/ner-eng-ie.crf-muc7.ser.gz  5555 &
 
 wait
