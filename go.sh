21c1c53f013fb0ed2cb0749777586648afd32dc0
brendano <brendano@08ee364a-b842-4d5f-b137-05461c37d77d>
2009-11-22 20:46:53 +0000

    moved all jars into lib/
    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@79 08ee364a-b842-4d5f-b137-05461c37d77d

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 here=$(dirname $0)
-java -mx1g -cp $here/bin:$here/stanford-ner-2008-05-07.jar:$here/stanford-parser-2008-10-26.jar:$here/commons-lang-2.4.jar analysis._Pipeline "$@"
+java -mx1g -cp $(print $here/**/*.jar | tr ' ' :):bin analysis._Pipeline "$@"
