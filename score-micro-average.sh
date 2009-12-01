4158976ffd4d9b0366e362eb9f78e2baf065b288
mheilman <mheilman@08ee364a-b842-4d5f-b137-05461c37d77d>
2009-12-01 21:46:55 +0000

    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@117 08ee364a-b842-4d5f-b137-05461c37d77d

--- score-micro-average.sh
+++ score-micro-average.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 
-PRECISION=`grep -P ' \d+/\d+\s+bad' log | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
-RECALL=`grep -P ' \d+/\d+\s+missing' log | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
+PRECISION=`grep -P ' \d+/\d+\s+bad' $1 | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
+RECALL=`grep -P ' \d+/\d+\s+missing' $1 | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
 
-
-
-perl -e "print \"PRECISION\t$PRECISION\nRECALL:\t$RECALL\nF1:\t\".($PRECISION*$RECALL*2)/($PRECISION+$RECALL).\"\n\";"
+echo "" | awk "END{print \"PRECISION\t$PRECISION\nRECALL:\t$RECALL\nF1:\t\"($PRECISION*$RECALL*2)/($PRECISION+$RECALL)\"\n\"}"
 
 
 
