79c74103606afa3817164eba606b16a44adc9c0c
brendano <brendano@08ee364a-b842-4d5f-b137-05461c37d77d>
2009-12-06 02:56:04 +0000

    mac doesn't have grep -P (sad)
    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@126 08ee364a-b842-4d5f-b137-05461c37d77d

--- score-micro-average.sh
+++ score-micro-average.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-PRECISION=`grep -P ' \d+/\d+\s+bad' $1 | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
-RECALL=`grep -P ' \d+/\d+\s+missing' $1 | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
+P=`grep -E '[0-9]+/[0-9]+ +bad' $1 | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
+R=`grep -E '[0-9]+/[0-9]+ +missing' $1 | sed 's/\// /' | awk '{num+=$1; denom+=$2} END{print 1-num/denom}'`
 
-echo "" | awk "END{print \"PRECISION:\t$PRECISION\nRECALL:\t$RECALL\nF1:\t\"($PRECISION*$RECALL*2)/($PRECISION+$RECALL)\"\n\"}"
+echo "" | awk "END{print \"PRECISION:\t$P\nRECALL:\t$R\nF1:\t\"($P*$R*2)/($P+$R)\"\n\"}"
 
 
 
