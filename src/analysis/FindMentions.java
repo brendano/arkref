06ae7caaeab664f26becdbc1082bce0e5a1ea4b2
mheilman <mheilman@08ee364a-b842-4d5f-b137-05461c37d77d>
2010-01-08 16:09:10 +0000

    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@141 08ee364a-b842-4d5f-b137-05461c37d77d

--- src/analysis/FindMentions.java
+++ src/analysis/FindMentions.java
@@ -1,9 +1,12 @@
 package analysis;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import parsestuff.TregexPatternFactory;
+import parsestuff.U;
 import data.Document;
-import data.Mention;
 import data.Sentence;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.tregex.TregexMatcher;
@@ -11,18 +14,26 @@ import edu.stanford.nlp.trees.tregex.TregexPattern;
 
 public class FindMentions {
 	public static void go(Document d) {
-		System.out.println("\n***  Find Mentions  ***\n");
-
-		String patS = "NP !>># NP"; //needs to be the maximum projection of a head word, or a conjunction
-		TregexPattern pat = TregexPatternFactory.getPattern(patS);
-		for (Sentence s : d.sentences()) {
-			TregexMatcher matcher = pat.matcher(s.rootNode());
-			while (matcher.find()) {
-				Tree match = matcher.getMatch();
+		U.pl("\n***  Find Mentions  ***\n");
+		for (Sentence s : d.sentences()){
+			for(Tree match: findMentionNodes(s.rootNode())){
 				d.newMention(s, match);
 			}
 		}
 	}
+	
+	public static List<Tree> findMentionNodes(Tree root){
+		List<Tree> res = new ArrayList<Tree>();
+		
+		String patS = "NP !>># NP"; //needs to be the maximum projection of a head word, or a conjunction
+		TregexPattern pat = TregexPatternFactory.getPattern(patS);
+		TregexMatcher matcher = pat.matcher(root);
+		while (matcher.find()) {
+			res.add(matcher.getMatch());
+		}
+		return res;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Document d = Document.loadFiles("/d/arkref/data/lcross");
 		FindMentions.go(d);
