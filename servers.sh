e9ce687581fd454bb4dd8d404fcd07175ccf8822
brendano <brendano@08ee364a-b842-4d5f-b137-05461c37d77d>
2010-01-18 03:53:18 +0000

    make run from built jar
    git-svn-id: svn+ssh://malbec/usr1/brendano/svn/arkref@170 08ee364a-b842-4d5f-b137-05461c37d77d

--- servers.sh
+++ servers.sh
@@ -1,5 +1,6 @@
+cd $(dirname $0)
 set -x
-java -server -mx1g -cp bin:lib/stanford-parser-2008-10-26.jar arkref.parsestuff.StanfordParserServer lib/englishFactored.ser.gz &
+java -server -mx1g -cp bin:arkref.jar:lib/stanford-parser-2008-10-26.jar arkref.parsestuff.StanfordParserServer lib/englishFactored.ser.gz &
 java -server -mx400m -cp lib/stanford-ner-2008-05-07.jar edu.stanford.nlp.ie.NERServer -loadClassifier lib/ner-eng-ie.crf-muc7.ser.gz  5555 &
 
 wait
